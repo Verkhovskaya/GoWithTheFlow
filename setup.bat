d1eb33618f1d85db2c81cb08ccdb5f49ceebd690
blitzingeagle <mcmorrischen@gmail.com>
2017-11-11 11:05:13 -0500

    setup

--- setup.bat
+++ setup.bat
@@ -1,4 +1,7 @@
 cd yolo-9000
 cat yolo9000-weights/x* > yolo9000-weights/yolo9000.weights
 md5 yolo9000-weights/yolo9000.weights
+cd darknet 
+git reset --hard b61bcf544e8dbcbd2e978ca6a716fa96b37df767
+make
 
