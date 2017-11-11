0bd45ca7c4f08ac3f52fe030c14a3a439d1e73e4
blitzingeagle <mcmorrischen@gmail.com>
2017-11-11 10:57:15 -0500

    setup

--- setup.bat
+++ setup.bat
@@ -1,6 +1,3 @@
 cd yolo-9000
-cat yolo9000-weights/x* > yolo9000-weights/yolo9000.weights # it was generated from split -b 95m yolo9000.weights
-md5 yolo9000-weights/yolo9000.weights # d74ee8d5909f3b7446e9b350b4dd0f44  yolo9000.weights
-cd darknet 
-make # Will run on CPU. For GPU support, scroll down!
+cat yolo9000-weights/x* > yolo9000-weights/yolo9000.weights
 
